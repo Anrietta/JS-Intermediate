1c7c3a4189610bf143e4aaa4fe3c650c26abb31d
Anrietta <sekereshai@gmai.com>
2025-06-16 17:16:10 +0300

    Add task on Inheritance

--- assets/js/OOP.js
+++ assets/js/OOP.js
@@ -117,35 +117,78 @@
         // В нашому випадку клас Moderator - дочірній клас, а клас User - батьківський або базовий клас
 
         // Щоб вказати що один клас успадковує/розширює інший треба написати extends "Назва батьківського класу"
-        // class Moderator extends User {
-        //     constructor (name, surname, age, isMale, email, permissions) {
-        //             // виклик конструктора батьківського класу (User) за кодовим словом super();
-        //             super(name, surname, age, isMale, email); // це те саме що ми написали new User() і передали всі ті властивості User які нам потрібні в класі Moderator
-        //             this._permissions = this.permissions;
-        //     }
-        //     //метод getFullName ми видалили щоб не повторюватись, бо він спадкується з User (в прототипах можна перевірити)
-        //     // а якщо ми хочемо щоб в модератора не було функції getFullName то можемо її перевизначити : getFullName() {}; - просто вписуєм пустим ніби в нього немає своєї реалізації
-        //     getFullName() {};  // в такому разі при виклику ми просто отримаємо undefined
+        class Moderator extends User {
+            constructor (name, surname, age, isMale, email, permissions) {
+                    // виклик конструктора батьківського класу (User) за кодовим словом super();
+                    super(name, surname, age, isMale, email); // це те саме що ми написали new User() і передали всі ті властивості User які нам потрібні в класі Moderator
+                    this._permissions = permissions;
+            }
+            //метод getFullName ми видалили щоб не повторюватись, бо він спадкується з User (в прототипах можна перевірити)
+            // а якщо ми хочемо щоб в модератора не було функції getFullName то можемо її перевизначити : getFullName() {}; - просто вписуєм пустим ніби в нього немає своєї реалізації
+            // getFullName() {};  // в такому разі при виклику ми просто отримаємо undefined
 
-        //     // функція, щоб наш модератор міг надсилати повідомлення-попередження користувачу
-        //     sendMessage(user, message) {
-        //         // модератор "ім'я" відправив повідомлення "ми його отримуємо як параметр" користувачу "імя користувача обєкта який ми отримали як параметр"
-        //         console.log(`Moderator ${this._firstName} send message "${message}" to user ${user._firstName}`);
-        //     }
+            // функція, щоб наш модератор міг надсилати повідомлення-попередження користувачу
+            sendMessage(user, message) {
+                // модератор "ім'я" відправив повідомлення "ми його отримуємо як параметр" користувачу "імя користувача обєкта який ми отримали як параметр"
+                console.log(`Moderator ${this._firstName} send message "${message}" to user ${user._firstName}`);
+            }
 
-        // } 
+        } 
 
-        // const moderator = new Moderator('Mod', 'Modovych', 28, false, 'mod@mail.com', {
-        //     bann: false,
-        //     readPrivate: false,
-        //     sendMessage: true
-        // });
+        const moderator = new Moderator('Mod', 'Modovych', 28, false, 'mod@mail.com', {
+            bann: false,
+            readPrivate: false,
+            sendMessage: true
+        });
 
-        // console.log(moderator.getFullName());  // Mod Modovych
-        // moderator.sendMessage(user, 'You have a warning'); // Moderator Mod send message "You have a warning" to user Test
+        console.log(moderator.getFullName());  // Mod Modovych
+        moderator.sendMessage(user, 'You have a warning'); // Moderator Mod send message "You have a warning" to user Test
 
 
         // Не забуваємо що спакування ззовні - це є під капотом прототипи (в JS класи спадкування і наслідування реалізовано через прототипи)
+
+
+        // Завдання на основі попередніх класів User та Moderator:
+        // Реалізувати клас Admin, який є модератором, і на додачу має:
+        // властивість category (1, 2),
+        // реалізує логіку (забанити або зняти бан з конкретного користувача)
+        //  - bann (user) => втсановити для нього бан тру
+        //  - unbann (user) => втсановити для нього бан фолс
+
+        // Створити об'єкт класу Admin, протестувати роботу методів bann, unbann 
+
+        class Admin extends Moderator {
+            constructor (name, surname, age, isMale, email, permissions, category) {
+                super(name, surname, age, isMale, email, permissions);
+                this._category = 'middle';
+            }
+            getBanned(user) {
+                user._isBanned = true;
+                console.log(`Admin ${this._firstName} banned user ${user._firstName}!`);
+
+            }
+            getUnbanned(user) {
+                user._isBanned = false;
+                console.log(`Admin ${this._firstName} unbanned user ${user._firstName}!`);
+            }
+        }
+
+        const admin = new Admin('Admin', 'Adminovych', 32, false, 'admin@mail.com', {
+            bann: true,
+            readPrivate: false,
+            sendMessage: true
+        })
+
+        console.log(admin.getFullName());  // Admin Adminovych
+        admin.getBanned(user);
+        console.log(user._isBanned);  // true  // Admin Admin banned user Test!
+        admin.getUnbanned(user);
+        console.log(user._isBanned);  // true  // Admin Admin unbanned user Test!
+        admin.sendMessage(user, 'Don"t do that'); // Moderator Admin send message "Don"t do that" to user Test
+
+
+
+
 
 
 
